fd97fb85be6b9c09b54d7b0b34c94798037c2f2b
itssabrinaa <sabrinacassone04@gmail.com>
2024-12-04 16:36:40 +0100

    Modifiche Entities
    Getters e Setters per alcune classi

--- BACKEND/backendDietiEstates/src/main/java/backendDietiEstates/backendDietiEstates/Entity/Agente.java
+++ BACKEND/backendDietiEstates/src/main/java/backendDietiEstates/backendDietiEstates/Entity/Agente.java
@@ -2,4 +2,14 @@ package backendDietiEstates.backendDietiEstates.Entity;
 
 public class Agente extends Utente {
 	private Agenzia agenzia;
+
+	//Getter and Setter for agenzia
+	public Agenzia getAgenzia() {
+		return agenzia;
+	}
+
+	public void setAgenzia(Agenzia agenzia) {
+		this.agenzia = agenzia;
+	}
+	
 }
--- BACKEND/backendDietiEstates/src/main/java/backendDietiEstates/backendDietiEstates/Entity/Amministratore.java
+++ BACKEND/backendDietiEstates/src/main/java/backendDietiEstates/backendDietiEstates/Entity/Amministratore.java
@@ -2,4 +2,13 @@ package backendDietiEstates.backendDietiEstates.Entity;
 
 public class Amministratore extends Utente {
 	private Agenzia agenzia;
+	
+	//Getter and Setter for agenzia
+	public Agenzia getAgenzia() {
+		return agenzia;
+	}
+
+	public void setAgenzia(Agenzia agenzia) {
+		this.agenzia = agenzia;
+	}
 }
