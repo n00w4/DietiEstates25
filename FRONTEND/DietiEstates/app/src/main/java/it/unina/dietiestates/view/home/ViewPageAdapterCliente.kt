db9deaf77fbe1d8b444b2435a8125bc15ad6533f
itssabrinaa <sabrinacassone04@gmail.com>
2025-01-17 20:28:43 +0100

    [FRONTEND] Correzione navigazione
    Navigazione per la home page del Cliente completata. Da fare la stessa cosa per gli altri tipi di utente.

--- FRONTEND/DietiEstates/app/src/main/java/it/unina/dietiestates/view/home/ViewPageAdapterCliente.kt
+++ FRONTEND/DietiEstates/app/src/main/java/it/unina/dietiestates/view/home/ViewPageAdapterCliente.kt
@@ -3,16 +3,19 @@ package it.unina.dietiestates.view.home
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.fragment.app.FragmentActivity
-import it.unina.dietiestates.view.profile.ProfiloFragment
-import it.unina.dietiestates.view.search.RicercaHomeFragment
+import it.unina.dietiestates.view.navigation.ProfiloNavFragment
+import it.unina.dietiestates.view.navigation.RicercaNavFragment
 
 class ViewPagerAdapterCliente(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            0 -> RicercaHomeFragment()
-            1 -> ProfiloFragment()
-            else -> RicercaHomeFragment() // Default case
+            //0 -> RicercaHomeFragment()
+            //1 -> ProfiloFragment()
+            //else -> RicercaHomeFragment() // Default case
+            0 -> RicercaNavFragment()
+            1 -> ProfiloNavFragment()
+            else -> RicercaNavFragment()
         }
     }
 
---
+++ FRONTEND/DietiEstates/app/src/main/java/it/unina/dietiestates/view/navigation/RicercaNavFragment.kt
@@ -0,0 +1,18 @@
+package it.unina.dietiestates.view.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import it.unina.dietiestates.R
+
+class RicercaNavFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.ricerca_nav_controller, container, false)
+    }
+}
