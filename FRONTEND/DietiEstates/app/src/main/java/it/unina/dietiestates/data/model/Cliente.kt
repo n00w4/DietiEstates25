3d7fc89067e42e93e1fa27ba817a30ccad1762de
n00w4 <luigicesaro2003@libero.it>
2025-01-24 16:06:41 +0100

    [FRONTEND] Salvataggio token login GitHub

--- FRONTEND/DietiEstates/app/src/main/java/it/unina/dietiestates/data/model/Cliente.kt
+++ FRONTEND/DietiEstates/app/src/main/java/it/unina/dietiestates/data/model/Cliente.kt
@@ -1,8 +1,8 @@
 package it.unina.dietiestates.data.model
 
 data class Cliente(
-    private var nome: String? = null,
-    private var cognome: String? = null,
-    private var email: String? = null,
-    private var password: String? = null
+    val nome: String?,
+    val cognome: String?,
+    val email: String?,
+    val password: String?
 )
