4644a55a1c0153f603012d8647dd9c31cf280cfc
n00w4 <luigicesaro2003@libero.it>
2025-01-29 16:00:44 +0100

    [FRONTEND] Migrazione a creazione Utenti per Gestore

--- FRONTEND/DietiEstates/app/src/main/java/it/unina/dietiestates/view/home/ViewPageAdapterGestore.kt
+++ FRONTEND/DietiEstates/app/src/main/java/it/unina/dietiestates/view/home/ViewPageAdapterGestore.kt
@@ -3,17 +3,15 @@ package it.unina.dietiestates.view.home
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.fragment.app.FragmentActivity
-import it.unina.dietiestates.view.navigation.CreaAgentiNavFragment
+import it.unina.dietiestates.view.navigation.CreaUtenteNavFragment
 import it.unina.dietiestates.view.navigation.ModificaPwdNavFragment
 import it.unina.dietiestates.view.navigation.ProfiloNavFragment
-import it.unina.dietiestates.view.profile.GestoreModificaPwdAdminFragment
-import it.unina.dietiestates.view.profile.ProfiloFragment
 
 class ViewPagerAdapterGestore(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            0 -> CreaAgentiNavFragment()
+            0 -> CreaUtenteNavFragment()
             1 -> ModificaPwdNavFragment()
             2 -> ProfiloNavFragment()
             else -> ProfiloNavFragment() // Default case
