64f4438653ff488f56a53fdf2850d124a7930246
itssabrinaa <sabrinacassone04@gmail.com>
2025-01-29 15:58:49 +0100

    [FRONTEND] Crea annuncio
    Funziona, fino alla chiamata al backend (c'è qualche problema con il formato del json).

--- FRONTEND/DietiEstates/app/src/main/java/it/unina/dietiestates/controller/crea_annuncio/CreaAnnuncioController.kt
+++ FRONTEND/DietiEstates/app/src/main/java/it/unina/dietiestates/controller/crea_annuncio/CreaAnnuncioController.kt
@@ -1,4 +1,57 @@
 package it.unina.dietiestates.controller.crea_annuncio
 
-class CreaAnnuncioController {
+import android.content.Context
+import android.widget.Toast
+import it.unina.dietiestates.data.dto.ApiResponse
+import it.unina.dietiestates.data.dto.SharedPrefManager
+import it.unina.dietiestates.data.model.Annuncio
+import it.unina.dietiestates.data.viewmodel.AnnuncioViewModel
+import it.unina.dietiestates.network.retrofit.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class CreaAnnuncioController(private val context: Context) {
+
+    fun creaAnnuncio(annuncioVM: AnnuncioViewModel){
+        val annuncio = annuncioVMToAnnuncio(annuncioVM)
+
+        val api = RetrofitClient.instance
+        api.insertAnnuncio(annuncio).enqueue(object : Callback<ApiResponse> {
+            override fun onResponse(call: Call<ApiResponse>, response: Response<ApiResponse>) {
+                when (response.code()) {
+                    200, 201 -> {
+                        val apiResponse = response.body()
+                        if (apiResponse != null) {
+                            Toast.makeText(context, "Annuncio creato correttamente", Toast.LENGTH_LONG).show()
+                        } else {
+                            Toast.makeText(context, "Errore: la risposta del server è incompleta", Toast.LENGTH_LONG).show()
+                        }
+                    }
+                    409 -> {
+                        Toast.makeText(context, "Esiste già un account con quest'email. Impossibile creare un nuovo account", Toast.LENGTH_SHORT).show()
+                    }
+                    500 -> {
+                        Toast.makeText(context, "Server error: ${response.errorBody()?.string()}", Toast.LENGTH_SHORT).show()
+                    }
+                    else -> {
+                        Toast.makeText(context, "Error: ${response.errorBody()?.string()}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
+                Toast.makeText(context, "Failed to connect to server: ${t.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun annuncioVMToAnnuncio(annuncioVM: AnnuncioViewModel) : Annuncio{
+        val annuncio = Annuncio(null, annuncioVM.titolo, annuncioVM.indirizzo, annuncioVM.immagine,
+            annuncioVM.descrizione, annuncioVM.dimensioni, annuncioVM.prezzo, annuncioVM.piano,
+            annuncioVM.numeroStanze, annuncioVM.classeEnergetica, annuncioVM.ascensore, annuncioVM.portineria,
+            annuncioVM.climatizzazione, annuncioVM.boxAuto, annuncioVM.terrazzo, annuncioVM.giardino,
+            annuncioVM.tipoAnnuncio, annuncioVM.posizione, SharedPrefManager.getUserEmail(context))
+        return annuncio
+    }
 }
--- FRONTEND/DietiEstates/app/src/main/java/it/unina/dietiestates/view/crea_annuncio/CreaAnnuncioResultFragment.kt
+++ FRONTEND/DietiEstates/app/src/main/java/it/unina/dietiestates/view/crea_annuncio/CreaAnnuncioResultFragment.kt
@@ -4,14 +4,35 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import it.unina.dietiestates.R
+import it.unina.dietiestates.controller.crea_annuncio.CreaAnnuncioController
+import it.unina.dietiestates.data.viewmodel.AnnuncioViewModel
 
 class CreaAnnuncioResultFragment : Fragment(){
+
+    private val annuncioVM: AnnuncioViewModel by activityViewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_crea_annuncio_result, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val controller = CreaAnnuncioController(requireContext())
+        controller.creaAnnuncio(annuncioVM)
+
+        val fineBtn = view.findViewById<Button>(R.id.fineButton)
+        fineBtn.setOnClickListener{
+            findNavController().navigate(R.id.action_creaAnnuncioResultFragment_to_creaAnnuncioFragment)
+        }
+    }
+
 }
