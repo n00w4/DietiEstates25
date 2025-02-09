878c7c8d4557e7ef5b48a9391651e2f5f3d7dca6
itssabrinaa <sabrinacassone04@gmail.com>
2025-02-09 17:09:04 +0100

    [DATABASE] valid password
    modificato regex per tutti i tipi di utente

--- DATABASE/database.sql
+++ DATABASE/database.sql
@@ -35,7 +35,7 @@ CREATE TABLE est.Agente (
     partitaIVA CHAR(11) REFERENCES est.Agenzia(partitaIVA) ON DELETE CASCADE,
 
     CONSTRAINT checkValidEmail CHECK (email ~ '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'),
-    CONSTRAINT checkValidPasswd CHECK (password ~ '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')
+    CONSTRAINT checkValidPasswd CHECK (password ~ '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+-=[]{}|;:,.<>?/])[A-Za-z\d@$!%*?&]{8,}$')
 );
 
 -- Table Amministratore
@@ -47,7 +47,7 @@ CREATE TABLE est.Amministratore (
     partitaIVA CHAR(11) REFERENCES est.Agenzia(partitaIVA) ON DELETE CASCADE,
 
     CONSTRAINT checkValidEmail CHECK (email ~ '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'),
-    CONSTRAINT checkValidPasswd CHECK (password ~ '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')
+    CONSTRAINT checkValidPasswd CHECK (password ~ '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+-=[]{}|;:,.<>?/])[A-Za-z\d@$!%*?&]{8,}$')
 );
 
 -- Table Gestore
@@ -60,8 +60,8 @@ CREATE TABLE est.Gestore (
     partitaIVA CHAR(11) REFERENCES est.Agenzia(partitaIVA) ON DELETE CASCADE,
 
     CONSTRAINT checkValidEmail CHECK (email ~ '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'),
-    CONSTRAINT checkValidPasswd CHECK (password ~ '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'),
-    CONSTRAINT checkValidPasswdAdmin CHECK (passwordAdmin ~ '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')
+    CONSTRAINT checkValidPasswd CHECK (password ~ '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+-=[]{}|;:,.<>?/])[A-Za-z\d@$!%*?&]{8,}$'),
+    CONSTRAINT checkValidPasswdAdmin CHECK (passwordAdmin ~ '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+-=[]{}|;:,.<>?/])[A-Za-z\d@$!%*?&]{8,}$')
 );
 
 -- View unificata per tutti gli utenti
